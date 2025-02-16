0e4b87a63e5d14b6296136c62a230307043aa499
Cristian Villalobos <cristianvillalobos666@gmail.com>
2025-02-15 23:20:44 -0300

    ADD CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';  // Importa el módulo de CORS
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // Permite cualquier dominio. Para producción, puedes especificar un dominio específico en lugar de '*'
+  });
   // Escucha en el puerto asignado por Vercel o usa el puerto local 3000
   await app.listen(process.env.PORT || 3000);
 }

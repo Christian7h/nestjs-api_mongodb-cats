736d0a458ea551c066de3b8e5540deb141e55914
Cristian Villalobos <cristianvillalobos666@gmail.com>
2025-02-15 22:57:59 -0300

    UPDATE main.ts process.env.port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // Escucha en el puerto asignado por Vercel o usa el puerto local 3000
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
